a47cfdeccff952fbcf0aaf56ad54cf892b270c8c
lEoN <yl.cao@maimemo.com>
2021-07-12 14:48:13 +0100

    fix: networking warnings

--- test/Network.swift
+++ test/Network.swift
@@ -6,19 +6,42 @@
 //
 import Foundation
 import Alamofire
+
 // url
-struct URL {
-    static let http = "http://"
-    static let https = "https://"
-    static let host = "mainote.maimemo.com/"
-    static let login = "api/auth/login"
-    static let logout = "api/auth/logout"
-    static let getAllNotes = "api/notes" // 全量
-    static let createNotes = "api/note"
-    static let delete = "api/notes/"
-    static let getNoteWithID = "api/notes"
-    static let updateNotes = "api/notes/"
+extension URL {
+    enum Scheme: String {
+        case http
+        case https
+    }
+
+    enum Host: String {
+        case main = "mainote.maimemo.com"
+        case www = "www.maimemo.com"
+    }
+
+    enum Path: String {
+        case login      = "/api/auth/login"
+        case logout     = "/api/auth/logout"
+        case notes      = "/api/notes"          // 全量
+        case createNote = "/api/note"
+        case delete     = "/api/notes/"
+    }
+
+    static func make(scheme: Scheme = .https, host: Host = .main, path: Path) -> URL? {
+        var comps = URLComponents()
+        comps.scheme = scheme.rawValue
+        comps.host = host.rawValue
+        comps.path = path.rawValue
+        return comps.url
+    }
+
+    func test() {
+        let url = URL.make(path: .login)
+        print("\(String(describing: url))")
+    }
+
 }
+
 // 解析JSON
 struct ServerDescription: Codable {
     struct Author: Codable {
@@ -89,7 +112,8 @@ struct UserInfo {
     var nid: String?
     var note: Note?
 }
-// 请求服务器
+
+// swiftlint:disable:next cyclomatic_complexity
 func requestAndResponse(userInfo: UserInfo? = nil, function: Function,
                         method: HTTPMethod, completion: @escaping (_: ServerDescription) -> Void) {
     var parameters: [String: Any?]?
@@ -100,34 +124,34 @@ func requestAndResponse(userInfo: UserInfo? = nil, function: Function,
     if method == .get {
         parameters = nil
     }
-    let url: String
+    let url: URL?
     switch function {
     case .login:
-        url = URL.https + URL.host + URL.login
+        url = URL.make(path: .login)
         parameters = ["identity": userInfo?.email,
                       "password": userInfo?.pwd]
     case .logout:
-        url = URL.https + URL.host + URL.logout
+        url = URL.make(path: .logout)
     case .getAllNotes:
-        url = URL.https + URL.host + URL.getAllNotes // 全量
+        url = URL.make(path: .notes)
     case .createNotes:
-        url = URL.https + URL.host + URL.createNotes
+        url = URL.make(path: .createNote)
         parameters = ["title": userInfo?.note?.title,
                       "content": userInfo?.note?.content,
                       "is_public": userInfo?.note?.status,
                       "checksum": userInfo?.note?.checksum,
                       "local_updated_at": userInfo?.note?.localUpdatedAt]
     case .delete:
-        guard let nid = userInfo?.nid else {
+        if userInfo?.nid == nil {
             return
         }
-        url = URL.https + URL.host + URL.delete + nid
+        url = URL.make(path: .delete)
     case .getNoteWithID:
-        url = URL.https + URL.host + URL.getNoteWithID
+        url = URL.make(path: .notes)
         parameters = ["identity": userInfo?.email,
                       "password": userInfo?.pwd]
     case .updateNotes:
-        url = URL.https + URL.host + URL.updateNotes
+        url = URL.make(path: .notes)
         parameters = ["identity": userInfo?.email,
                       "password": userInfo?.pwd]
         parameters = ["title": userInfo?.note?.title,
@@ -136,22 +160,42 @@ func requestAndResponse(userInfo: UserInfo? = nil, function: Function,
                       "checksum": userInfo?.note?.checksum,
                       "local_updated_at": userInfo?.note?.localUpdatedAt]
     }
-    AF.request(url, method: method, parameters: (parameters ?? nil) as? Parameters,
-               encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-        // 大括号是closure，匿名函数，response是参数，捕获上下文
-        switch response.result {
-        case .success(let json):
-                guard let data = response.data else { return }
-                // 解析JSON和捕获异常
-                do {
-                    let serverDescription = try JSONDecoder().decode(ServerDescription.self, from: data)
-                    completion(serverDescription)
-                    print(json)
-                } catch let jsonErr {
-                    print("json 解析出错 : ", jsonErr)
-                }
-        case .failure(let error):
-                print(error)
+
+    var params = [String: Any]()
+    for (key, value) in parameters ?? [:] {
+        if let value = value {
+            params[key] = value
         }
     }
+
+    // TODO: optional URL
+    let urlString = url?.absoluteString
+    var request: DataRequest?
+    request =
+        AF.request(urlString!,
+                   method: method,
+                   parameters: params,
+                   encoding: JSONEncoding.default,
+                   headers: headers).responseJSON { response in
+                    DispatchQueue.global().async {
+                        if let curl = request?.cURLDescription() {
+                            print("API: \(curl)")
+                        }
+                    }
+                    // 大括号是closure，匿名函数，response是参数，捕获上下文
+                    switch response.result {
+                    case .success(let json):
+                        guard let data = response.data else { return }
+                        // 解析JSON和捕获异常
+                        do {
+                            let serverDescription = try JSONDecoder().decode(ServerDescription.self, from: data)
+                            completion(serverDescription)
+                            print(json)
+                        } catch let jsonErr {
+                            print("json 解析出错 : ", jsonErr)
+                        }
+                    case .failure(let error):
+                        print(error)
+                    }
+                   }
 }
